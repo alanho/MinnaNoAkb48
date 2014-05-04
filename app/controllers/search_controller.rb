cce164f8604fa530b5d9098313435b355d648020
Alan Ho <alanho@gmail.com>
2014-05-04 14:17:42 +0800

    update

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def index
     if params[:q]
-      @response = Song.search(query: {match: {lyric_jp: params[:q]}, analyzer: "kuromoji"}, highlight: { fields: { lyric_jp: {}} }).page(params[:page])
+      @response = Song.search(query: {match: {lyric_jp: params[:q]}}, highlight: { fields: { lyric_jp: {}} }).page(params[:page])
     end
   end
 end
