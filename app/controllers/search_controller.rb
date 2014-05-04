b95b38f3ee2adebe78f2dd2f28e9af0d17e6c628
Alan Ho <alanho@gmail.com>
2014-05-05 03:03:02 +0800

    updates

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def index
     if params[:q]
-      @response = Song.search(query: {match: {lyric_jp: params[:q]}}, highlight: { fields: { lyric_jp: {}} }).page(params[:page])
+      @response = Song.search(query: {match: {lyric_jp: params[:q]}}, highlight: { fields: { lyric_jp: {}} },).page(params[:page]).per(100)
     end
   end
 end
